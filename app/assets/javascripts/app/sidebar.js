0e4e19d0c180b0a05d4e30e6f18643532956a9af
Michael Dawson <email.michaeldawson@gmail.com>
2016-08-10 12:17:30 +1000

    Some tidying

--- app/assets/javascripts/app/sidebar.js
+++ app/assets/javascripts/app/sidebar.js
@@ -1,20 +1,22 @@
 $(document).on('turbolinks:load', function() {
-  var $window = $(window),
-      $socialIcons = $('.sidebar .social'),
-      $sidebarContent = $('.sidebar .content');
+  (function setupVerticalScrollHandling() {
+    var $window = $(window),
+        $socialIcons = $('.sidebar .social'),
+        $sidebarContent = $('.sidebar .content');
 
-  var triggerHeight = $sidebarContent.height() + parseInt($socialIcons.css('bottom')) + 60;
+    var triggerHeight = $sidebarContent.height() + parseInt($socialIcons.css('bottom')) + 60;
 
-  var repositionSocialIconsIfUnderneathContent = function(event) {
-    var height = $window.height();
+    var repositionSocialIconsIfUnderneathContent = function(event) {
+      var height = $window.height();
 
-    if(height < triggerHeight){
-      $socialIcons.css('position', 'static');
-    } else {
-      $socialIcons.css('position', 'absolute');
+      if(height < triggerHeight){
+        $socialIcons.css('position', 'static');
+      } else {
+        $socialIcons.css('position', 'absolute');
+      }
     }
-  }
 
-  repositionSocialIconsIfUnderneathContent();
-  addResizeListener(repositionSocialIconsIfUnderneathContent);
+    repositionSocialIconsIfUnderneathContent();
+    addResizeListener(repositionSocialIconsIfUnderneathContent);
+  })()
 });
