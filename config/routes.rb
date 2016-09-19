dc0df741a62265d6f184f5e7841c7e87df2e71fe
Michael Dawson <mdawson@marketplacer.com>
2016-09-19 21:06:34 +1000

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
 
-  devise_for :users
+  devise_for :users, skip: [:sessions, :registrations, :passwords, :unlocks]
+  as :user do
+    get 'login' => 'sessions#new', as: :new_user_session
+    post 'login' => 'sessions#create', as: :user_session
+    match 'logout' => 'sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+  end
 end
